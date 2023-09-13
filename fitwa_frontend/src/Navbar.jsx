e1b8c7cb4c4fd8e4a3440daaeb8a05bb5e53aab8
sorawit nunsatit <108317085+sorawitkku@users.noreply.github.com>
2023-09-13 19:46:21 +0700

    add

---
+++ fitwa_frontend/src/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div className="Navbar">
+            <header className="Navbar_main">Fitwa</header>
+            <p className="Navbar_gymnearme">Gym near Me</p>
+        </div>
+    );
+}
+
+export default Navbar;
