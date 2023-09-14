81e0d4e7472458b8a74de42f67ee0e60da9f3dd2
sorawit nunsatit <108317085+sorawitkku@users.noreply.github.com>
2023-09-15 01:36:48 +0700

    add

--- fitwa_frontend/src/App.jsx
+++ fitwa_frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './Navbar'
+import Header from './component/Header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <div>
-        <Navbar />
+        <Header />
       </div>
     
     </>
---
+++ fitwa_frontend/src/component/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { FiActivity } from "react-icons/fi";
+
+
+
+function Header(){
+
+
+    return(
+        <div className="header">
+            <div className="container">
+                <div className="header-con">
+                    <div className="logo">
+                        <a href="#">Fitwa<FiActivity/></a>
+                    </div>
+                    <ul className="menu">
+                        <li>
+                            <a href="menu-link">gym near me</a>
+                        </li>
+                        <li>
+                            <a href="menu-link">about us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
