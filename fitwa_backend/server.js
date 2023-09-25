162ea14e19055273248ed1bbdaf7f9ad427ff60e
NongBond <thanakorn.pr@kkumail.com>
2023-09-25 16:52:00 +0700

    addCors

--- fitwa_backend/server.js
+++ fitwa_backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const PORT = 6969
 const db = require("./models")
@@ -7,6 +8,7 @@ const postRouter = require("./routers/Posts")
 
 app.use(express.json());
 app.use("/posts", postRouter);
+app.use(cors());
 
 
 db.sequelize.sync().then(app.listen(PORT, () => {
