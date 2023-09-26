4a319270be191be1e12c13a70b17ef274ed92bbe
sorawit nunsatit <108317085+sorawitkku@users.noreply.github.com>
2023-09-26 21:47:10 +0700

    ph auth

--- fitwa_frontend/src/component/SignInBox.jsx
+++ fitwa_frontend/src/component/SignInBox.jsx
@@ -1,35 +1,92 @@
 import React, { useState } from 'react';
-import "./SignInBox.css"
+import './SignInBox.css';
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
+import { RecaptchaVerifier, signInWithPhoneNumber, PhoneAuthProvider } from 'firebase/auth'; // Import PhoneAuthProvider
+import { auth } from '../firebase.config';
+import toast, { Toaster } from 'react-hot-toast';
 
 function SignInBox() {
-  // State to store user input
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [otp, setOtp] = useState('');
 
-  // Function to handle form submission
-  const handleSignIn = (e) => {
+  // Create a state variable to store the confirmation result
+  const [confirmationResult, setConfirmationResult] = useState(null);
+
+  const handleSignIn = async (e) => {
     e.preventDefault();
-    // You can add your authentication logic here
-    console.log('Phone Number:', phoneNumber);
+    try {
+      const appVerifier = new RecaptchaVerifier('recaptcha-container', {
+        size: 'invisible',
+        callback: (response) => {
+          // Captcha callback, called when captcha verification is successful
+          // Send OTP to the phone number
+          sendOTP();
+        },
+        'expired-callback': () => {
+          // Captcha expired callback
+          // Handle expired captcha here
+        },
+      });
+
+      const formatPh = '+66' + phoneNumber; // Set the country code to '+66' for Thailand
+      const provider = new PhoneAuthProvider(auth);
+
+      // Initiate phone number verification
+      const confirmation = await signInWithPhoneNumber(provider, formatPh, appVerifier);
+
+      setConfirmationResult(confirmation);
+      
+      toast.success('OTP Sent Successfully!');
+    } catch (error) {
+      console.error(error);
+      toast.error('Failed to send OTP.');
+    }
+  };
+
+  const handleVerifyOTP = async () => {
+    try {
+      toast.success('OTP Verified Successfully!');
+    } catch (error) {
+      console.error(error);
+      toast.error('Failed to verify OTP.');
+    }
   };
 
   return (
-    <div className='sign-container'>
+    <div className="sign-container">
+      <Toaster toastOptions={{ duration: 2000 }} />
       <div className="sign-in-box">
         <h2>Sign In with Phone Number</h2>
         <form onSubmit={handleSignIn}>
           <div className="form-group">
             <label htmlFor="phoneNumber">Phone Number:</label>
-            <input
-              type="tel"
-              id="phoneNumber"
+            <PhoneInput
+              country={'th'} // Set the default country to Thailand
+              inputProps={{
+                id: 'phoneNumber',
+                required: true,
+                placeholder: 'e.g., 610526499'
+              }}
               value={phoneNumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}
-              required
-              placeholder="e.g., +66610526499"
+              onChange={setPhoneNumber}
             />
           </div>
-          <button type="submit">Sign In</button>
+          <div id="recaptcha-container"></div>
+          <button type="submit">Send OTP</button>
         </form>
+        {confirmationResult && (
+          <div>
+            <p>Enter OTP:</p>
+            <input
+              type="text"
+              value={otp}
+              onChange={(e) => setOtp(e.target.value)}
+              placeholder="Enter OTP"
+            />
+            <button onClick={handleVerifyOTP}>Verify OTP</button>
+          </div>
+        )}
       </div>
     </div>
   );
--- fitwa_frontend/src/firebase.config.js
+++ fitwa_frontend/src/firebase.config.js
@@ -1,20 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA2gphGNdegYUjp7r8N_bgCBtwxfd6N3rk",
-  authDomain: "fitwa-197c5.firebaseapp.com",
-  projectId: "fitwa-197c5",
-  storageBucket: "fitwa-197c5.appspot.com",
-  messagingSenderId: "1023622810382",
-  appId: "1:1023622810382:web:0e93ad7ade082d9114255f",
-  measurementId: "G-13J3Q69HPE"
+    apiKey: "AIzaSyA2gphGNdegYUjp7r8N_bgCBtwxfd6N3rk",
+    authDomain: "fitwa-197c5.firebaseapp.com",
+    projectId: "fitwa-197c5",
+    storageBucket: "fitwa-197c5.appspot.com",
+    messagingSenderId: "1023622810382",
+    appId: "1:1023622810382:web:0e93ad7ade082d9114255f",
+    measurementId: "G-13J3Q69HPE"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = app.auth();
