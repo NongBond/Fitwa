2746f7eff4ded60475083756a9c4778b9a0b9673
NongBond <thanakorn.pr@kkumail.com>
2023-09-26 17:50:06 +0700

    addAccessToDB

--- fitwa_frontend/src/component/FeedRender.jsx
+++ fitwa_frontend/src/component/FeedRender.jsx
@@ -1,21 +1,36 @@
-import React from "react";
+//import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import "./FeedRender.css";
 
-function FeedRender({ posts }) {
+function FeedRender() {
   const post = [{postTitle:"Find friend",
                 selectedGym:"NP Park",
               description:"Anyone want to go to NP Park at 2PM"}]
+  // const [listOfPost, setListOfPost] = useState([]);
+  // useEffect(() => {axios.get("http://localhost:6969/main") // get all the post in database
+  // .then((response) => {
+  //   setListOfPost(response.data); //
+  // })},[])
+
     return (
-      <div className="feed-render-con">
-        <h2>Feed</h2>
-        {posts.map((post, index) => ( 
-          <div key={index} className="post">
-            <h3>{post.postTitle}</h3>
-            console.log(postTitle);
-            <p>Gym Name: {post.selectedGym}</p>
-            <p>Description: {post.description}</p>
+      <div className="feed-render-com">
+        {post.map((value, key) => { // map through each object(post)
+          return <div className="post">
+            <div className="title">{value.postTitle}</div>
+            <div className="post_description">{value.selectedGym}</div>
+            <div className="gym_name">{value.description}</div>
           </div>
-        ))}
+        })}
+
+        {/* {listOfPost.map((value, key) => { // map through each object(post)
+          return <div className="post">
+            <div className="title">{value.title}</div>
+            <div className="post_description">{value.gymName}</div>
+            <div className="gym_name">{value.postDescription}</div>
+            <br></br>
+          </div>
+        })} */}
       </div>
     );
   }
