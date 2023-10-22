291c87d7672fbc18e58eee3d6ebcc7e61f0b349b
sorawit nunsatit <108317085+sorawitkku@users.noreply.github.com>
2023-10-22 23:47:21 +0700

    add button

--- fitwa_frontend/src/App.jsx
+++ fitwa_frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Login from './component/Login'
-import { redirect } from 'react-router-dom'
+import { Link, redirect } from 'react-router-dom'
 
 
 function App() {
   const [count, setCount] = useState(0)
   return (
-    <div className='container'></div>
+    <div>
+      <Link to='/SignUp' className='SignUp'>SignIn</Link>
+      <Link to='/Login' className='Login'>Login</Link>
+    </div>
   )
 }
 
