9a67b6786ba0935265d555e94a79a2f16f7d99a3
NongBond <thanakorn.pr@kkumail.com>
2023-10-24 15:45:48 +0700

    addSyncLogicForUser

--- fitwa_mongoDb/controllers/User.js
+++ fitwa_mongoDb/controllers/User.js
@@ -3,6 +3,42 @@ const {ObjectId} = require("mongodb")
 const admin = require("firebase-admin");
 const serviceAccount = require("../../firebase/fitwa-197c5-firebase-adminsdk-p74f5-ed54d829ee.json");
 
+// const mongoose = require('mongoose');
+
+// const mongoURI = 'mongodb://localhost:27017/your_mongodb_database';
+// mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open', () => {
+//   console.log('Connected to MongoDB');
+// });
+
+// const mongooseSchema = new mongoose.Schema({
+//     name: String,
+//     age: Number,
+//   });
+  
+//   const MongooseModel = mongoose.model('MongooseModel', mongooseSchema);
+
+// const PORT = process.env.PORT || 6969;
+// const URI = process.env.ATLAS_URI;
+
+// exports.syncFirestoreToMongo = functions.firestore
+//   .document('your_firestore_collection/{docId}')
+//   .onWrite(async (change, context) => {
+//     const docData = change.after.exists ? change.after.data() : null;
+//     const docId = context.params.docId;
+
+//     if (!docData) {
+//       // Document deleted in Firestore, delete it in MongoDB
+//       await MongooseModel.findByIdAndRemove(docId);
+//     } else {
+//       // Update or insert the document in MongoDB
+//       await MongooseModel.findByIdAndUpdate(docId, docData, { upsert: true });
+//     }
+//   });
+
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -92,39 +128,42 @@ const getUsers = async (req, res) => {
   }
 }
 
-// async function transferData(){
-//     console.log("transfer")
-//     const collectionRef = firestore.collection("users");
-//     try{
-//         const querySnapshot = await collectionRef.get();
+// const functions = require('firebase-functions');
+// const admin = require('firebase-admin');
+// const MongoClient = require('mongodb').MongoClient;
 
+// admin.initializeApp();
 
-//         querySnapshot.forEach((doc) => {
-//           const data = doc.data();
-//           console.log(data)
-//           //data.then()
-//           const firestoreId = doc.id; // Get the Firestore document ID
-//         //   const mongoId = new ObjectId(firestoreId);
-//         //   console.log(mongoId)
-    
-//           // Create a new document with the Firestore ID and save it to MongoDB
-//           const newData = new userModel({
-//             name: data.Name,
-//             surname: data.Surname,
-//             age: data.age,
-//             email: data.email,
-//             sex: data.sex,
-//             userId: firestoreId, // Set the Firestore document ID
-//             // Add other fields as needed
-//           });
-    
-//           newData.save();
-//         });
+// const db = admin.firestore();
+// const mongoURI = 'mongodb://localhost:27017';
+// const dbName = 'your_mongodb_database';
 
-//     }catch(err){
-//         console.error('Error getting documents from Firestore:', err);
+// exports.syncFirestoreToMongo = functions.firestore
+//   .document('your_firestore_collection/{docId}')
+//   .onWrite(async (change, context) => {
+//     const docData = change.after.exists ? change.after.data() : null;
+//     const docId = context.params.docId;
+
+//     // If the document was deleted in Firestore, delete it in MongoDB
+//     if (!docData) {
+//       const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+//       await client.connect();
+//       const mongodb = client.db(dbName);
+//       await mongodb.collection('your_mongodb_collection').deleteOne({ _id: docId });
+//       await client.close();
+//     } else {
+//       // Update or insert the document in MongoDB
+//       const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+//       await client.connect();
+//       const mongodb = client.db(dbName);
+//       await mongodb.collection('your_mongodb_collection').updateOne(
+//         { _id: docId },
+//         { $set: docData },
+//         { upsert: true }
+//       );
+//       await client.close();
 //     }
-// }
+//   });
 
 
 module.exports = {transferData, findUserViaFirebase, findUser, getUsers};
